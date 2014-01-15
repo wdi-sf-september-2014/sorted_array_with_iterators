18d3098e4fdcf65baa203d0c6840dd19699de242
Alex Notov <anotov@gmail.com>
2014-01-14 23:48:25 -0800

    Spec with solution

--- sorted_array_spec.rb
+++ sorted_array_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require './sorted_array.rb'
+require 'pry'
 
 shared_examples "yield to all elements in sorted array" do |method|
     specify do 
@@ -28,45 +29,73 @@ describe SortedArray do
       describe :map do
         it 'the original array should not be changed' do
           original_array = sorted_array.internal_arr
-          expect { sorted_array.map {|el| el } }.to_not change { original_array }
+          expect { sorted_array.map {|el| el * 2 } }.to_not change { original_array }
         end
 
-        it_should_behave_like "yield to all elements in sorted array", :map
+        it_should_behave_like 'yield to all elements in sorted array', :map
 
-        it 'creates a new array containing the values returned by the block' do
-          pending "fill this spec in with a meaningful example"
+        it 'should return an array' do
+          sorted_array.map {|el| el }.class.should eq Array
+        end
+
+        its 'array should not be the original array' do
+          undesired_obj_id = sorted_array.internal_arr.object_id
+          sorted_array.map {|el| el }.object_id.should_not eq undesired_obj_id
+        end
+
+        its 'returned array contains the values returned by the block' do
+          sorted_array.map { |el| el * 2 }.should eq [4,6,8,14,18]
         end
       end
     end
 
     describe "that update the original array" do
       describe :map! do
-        it 'the original array should be updated' do
-          pending "fill this spec in with a meaningful example"
-        end
-
         it_should_behave_like "yield to all elements in sorted array", :map!
 
+        it 'should return an array' do
+          sorted_array.map! {|el| el }.class.should eq Array
+        end
+
+        its 'array should be the original array' do
+          desired_obj_id = sorted_array.internal_arr.object_id
+          sorted_array.map! {|el| el }.object_id.should eq desired_obj_id
+        end
+
         it 'should replace value of each element with the value returned by block' do
-          pending "this is just the same as the example above"
+          sorted_array.map! { |el| el * 2 }.should eq [4,6,8,14,18]
         end
       end
     end
   end
 
   describe :find do
-    it_should_behave_like "yield to all elements in sorted array", :find
+    describe 'given a block' do
+      it 'should return the element for which the block is true' do
+        # let's see if we can get a multiple of 7
+        sorted_array.find { |el| el % 7 == 0 }.should eq 7
+      end
 
-    it "does not currently have any examples for it" do
-      pending "define some examples by looking up http://www.ruby-doc.org/core-2.1.0/Enumerable.html#method-i-find"
+      context 'cannot find the element' do
+        it 'should return nil' do
+          # a multiple of 10 should not exist
+          sorted_array.find { |el| el % 10 == 0 }.should be_nil
+        end
+      end
     end
   end
 
   describe :inject do
-    it_should_behave_like "yield to all elements in sorted array", :inject
+    context 'when passed no accumulator' do
+      it 'should accumulate from the first element' do
+        sorted_array.inject { |acc,el| acc + el }.should == 25
+      end
+    end
 
-    it "does not currently have any examples for it" do
-      pending "define some examples by looking up http://www.ruby-doc.org/core-2.1.0/Enumerable.html#method-i-inject"
+    context 'when passed an accumulator' do
+      it 'should accumulate starting with that value' do
+        sorted_array.inject(5) { |acc,el| acc + el }.should == 30
+      end
     end
   end
 end
